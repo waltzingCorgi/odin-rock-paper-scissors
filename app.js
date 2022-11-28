6bc01ab4125836cd6ddc9ff9d2e8e22f4052ea44
Patrick <16686610+waltzingCorgi@users.noreply.github.com>
2022-11-27 20:37:29 -0600

    Implement win/lose logic

--- app.js
+++ app.js
@@ -10,3 +10,48 @@ function getComputerChoice() {
             return "SCISSORS";
     }
 }
+
+function playRound(playerSelection, computerSelection) {
+    let winner;
+
+    switch(true) {
+        case (playerSelection === "ROCK" && computerSelection === "ROCK"):
+            winner = "Tie";
+            break;
+        case (playerSelection === "ROCK" && computerSelection === "PAPER"):
+            winner = "Computer";
+            break;
+        case (playerSelection === "ROCK" && computerSelection === "SCISSORS"):
+            winner = "Player";
+            break;
+        case (playerSelection === "PAPER" && computerSelection === "ROCK"):
+            winner = "Player";
+            break;
+        case (playerSelection === "PAPER" && computerSelection === "PAPER"):
+            winner = "Tie";
+            break;
+        case (playerSelection === "PAPER" && computerSelection === "SCISSORS"):
+            winner = "Computer";
+            break;
+        case (playerSelection === "SCISSORS" && computerSelection === "ROCK"):
+            winner = "Tie";
+            break;
+        case (playerSelection === "SCISSORS" && computerSelection === "PAPER"):
+            winner = "Player";
+            break;
+        case (playerSelection === "SCISSORS" && computerSelection === "SCISSORS"):
+            winner = "Computer"
+            break;
+    }
+
+    switch(winner) {
+        case "Player":
+            return `You win! ${playerSelection} beats ${computerSelection}.`
+        case "Computer":
+            return `You lose! ${computerSelection} beats ${playerSelection}.`
+        case "Tie":
+            return `Tie! ${playerSelection} ties ${computerSelection}.`
+        default:
+            return "Error!";
+    }
+}
